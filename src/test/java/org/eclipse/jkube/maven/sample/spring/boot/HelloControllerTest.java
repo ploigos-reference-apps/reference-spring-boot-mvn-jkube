ef9faac91c19f1377c30d4c641ee92bc0a63b1e6
Ian Tewksbury <itewk@redhat.com>
2021-08-05 11:49:49 -0400

    add unit test

---
+++ src/test/java/org/eclipse/jkube/maven/sample/spring/boot/HelloControllerTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at:
+ *
+ *     https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.jkube.maven.sample.spring.boot;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class HelloControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        mockMvc = null;
+    }
+
+    @Test
+    void shouldReturnHello() throws Exception {
+        final ResultActions result = mockMvc.perform(get("/"));
+
+        result.andExpect(status().isOk());
+        result.andExpect(content().string("Greetings from Spring Boot with YAML files!!"));
+    }
+
+}
