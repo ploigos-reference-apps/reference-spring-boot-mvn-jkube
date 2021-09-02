ee56d5e91151e091b7f06d23e40bf29bfadfd5a3
Ian Tewksbury <itewk@redhat.com>
2021-09-02 15:27:23 -0400

    maybe working now?

--- src/test/java/org/eclipse/jkube/maven/sample/spring/boot/RunCucumberIT.java
+++ src/test/java/org/eclipse/jkube/maven/sample/spring/boot/RunCucumberIT.java
@@ -1,12 +1,9 @@
 package org.eclipse.jkube.maven.sample.spring.boot;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-
 import org.junit.runner.RunWith;
 
 @io.cucumber.junit.platform.engine.Cucumber
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
 public class RunCucumberIT {
 }
